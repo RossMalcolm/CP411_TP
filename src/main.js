3493d0213a68e47c06626145bd08a952617d511d
Ross Malcolm <rossmalc75@gmail.com>
2020-12-02 17:25:02 -0500

    Cube appearing

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+
+// Alternatively, to parse a previously loaded JSON structure
+
+camera.position.z = 5;
+
+function animate(){
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+animate();
