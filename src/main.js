e6681b96dc1fce85eeb51e6e764045b621d564a8
Riley Voigt <voig4620@mylaurier.ca>
2020-12-05 23:28:21 -0500

    Camera and lighting working for all models

--- src/main.js
+++ src/main.js
@@ -16,12 +16,13 @@ var scene3 = new THREE.Scene();
 
 
 
+
 function main() {
 
 	document.body.appendChild(renderer.domElement);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
-	camera.position.z = 200;
+	camera.position.z = 250;
 
 	const light1_x = 1;
     const light1_y = 1;
@@ -30,53 +31,79 @@ function main() {
     const light1_intensity = 1;
     const light1 = new THREE.DirectionalLight(light1_color, light1_intensity);
     light1.position.set(light1_x, light1_y, light1_z);
-    scene.add(light1);
+    const light2 = new THREE.DirectionalLight(light1_color, light1_intensity);
+    light2.position.set(light1_x, light1_y, light1_z);
+    const light3 = new THREE.DirectionalLight(light1_color, light1_intensity);
+    light3.position.set(light1_x, light1_y, light1_z);
 
+    scene.add(light1);
+    scene.add(directionalLight);
     scene.add(light);
 	loader.load( '../models/buster/scene.gltf', function ( gltf ) {
+		gltf.scene.scale.multiplyScalar(1.2); // adjust scalar factor to match your scene scale
+        gltf.scene.position.x = 1; // once rescaled, position the model where needed
+        gltf.scene.position.z = -35;
+        gltf.scene.position.y = 35;
 
-	scene.add( gltf.scene );
+		scene.add( gltf.scene );
 
 	}, undefined, function ( error ) {
 
 	console.error( error );
 
 	} );
+    
 
-	
-
-	scene2.add(light1);
+	scene2.add(light2);
+	scene2.add(directionalLight);
 	scene2.add(light);
 	loader.load( '../models/headphones/scene.gltf', function(gltf){
+		gltf.scene.scale.multiplyScalar(3/4); // adjust scalar factor to match your scene scale
+        gltf.scene.position.x = 1; // once rescaled, position the model where needed
+        gltf.scene.position.z = -35;
+        gltf.scene.position.y = -25;
+
 		scene2.add(gltf.scene);
 	}, undefined, function (error) {
 		console.error(error);
 	});
 
-	scene3.add(light1);
+	
+
+	scene3.add(light3);
+	scene3.add(directionalLight);
 	scene3.add(light);
 	loader.load( '../models/gun/scene.gltf', function(gltf){
+		gltf.scene.scale.multiplyScalar(45); // adjust scalar factor to match your scene scale
+        gltf.scene.position.x = 1; // once rescaled, position the model where needed
+        gltf.scene.position.z = -35;
+        gltf.scene.position.y = 0;
+
 		scene3.add(gltf.scene);
 	}, undefined, function (error) {
 		console.error(error);
 	});
 
+
+
+
+
 	var backgroundButton = document.getElementById('scene');
 	backgroundButton.addEventListener('click', function(){
 		currentScene = scene;
-   		camera.position.z = 200;
+   		
 	});
 
 	var backgroundButton = document.getElementById('scene2');
 	backgroundButton.addEventListener('click', function(){
 		currentScene = scene2;
-   		camera.position.z = 200;
+   		
 	});
 
 	var backgroundButton = document.getElementById('scene3');
 	backgroundButton.addEventListener('click', function(){
 		currentScene = scene3;
-   		camera.position.z = 400;
+   		
 	});
 
 
