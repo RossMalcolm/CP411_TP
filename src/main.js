a05e5045a9ea32b5ab5eb16b11a64313a65c4fe9
Ross Malcolm <rossmalc75@gmail.com>
2020-12-06 01:40:11 -0500

    3 scenes turned into one

--- src/main.js
+++ src/main.js
@@ -5,22 +5,21 @@ const cube = new THREE.Mesh( geometry, material );
 var currentScene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias:true});
-const light = new THREE.AmbientLight( 0x404040 );
+const light = new THREE.AmbientLight( 0x404040, 25);
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 const loader = new THREE.GLTFLoader();
+var car1 = new THREE.Object3D();
+var car2 = new THREE.Object3D();
+var car3 = new THREE.Object3D();
 var scene = new THREE.Scene();
-var scene2 = new THREE.Scene();
-var scene3 = new THREE.Scene();
-
-
-
-
 
 function main() {
 
 	document.body.appendChild(renderer.domElement);
 	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	document.body.addEventListener('keydown', keyPressed);
 
 	camera.position.z = 250;
 
@@ -35,6 +34,8 @@ function main() {
     light2.position.set(light1_x, light1_y, light1_z);
     const light3 = new THREE.DirectionalLight(light1_color, light1_intensity);
     light3.position.set(light1_x, light1_y, light1_z);
+
+
     const spotLight = new THREE.SpotLight( 0xffffff );
 	spotLight.position.set( 100, 1000, 100 );
 
@@ -46,40 +47,21 @@ function main() {
 	spotLight.shadow.camera.near = 500;
 	spotLight.shadow.camera.far = 4000;
 	spotLight.shadow.camera.fov = 30;
-	const spotLight2 = new THREE.SpotLight( 0xffffff );
-	spotLight2.position.set( 100, 1000, 100);
 
-	spotLight2.castShadow = true;
-
-	spotLight2.shadow.mapSize.width = 1024;
-	spotLight2.shadow.mapSize.height = 1024;
-
-	spotLight2.shadow.camera.near = 500;
-	spotLight2.shadow.camera.far = 4000;
-	spotLight2.shadow.camera.fov = 30
-	const spotLight3 = new THREE.SpotLight( 0xffffff );
-	spotLight3.position.set( 100, 1000, 100 );
-
-	spotLight3.castShadow = true;
-
-	spotLight3.shadow.mapSize.width = 1024;
-	spotLight3.shadow.mapSize.height = 1024;
-
-	spotLight3.shadow.camera.near = 500;
-	spotLight3.shadow.camera.far = 4000;
-	spotLight3.shadow.camera.fov = 30
 
     scene.add(light1);
     scene.add(spotLight);
     scene.add(directionalLight);
     scene.add(light);
-	loader.load( '../models/buster/scene.gltf', function ( gltf ) {
-		gltf.scene.scale.multiplyScalar(1.2); // adjust scalar factor to match your scene scale
-        gltf.scene.position.x = 1; // once rescaled, position the model where needed
-        gltf.scene.position.z = -35;
-        gltf.scene.position.y = 35;
 
-		scene.add( gltf.scene );
+	loader.load( '../models/car1/scene.gltf', function ( gltf ) {
+		car1 = gltf.scene;
+		car1.scale.multiplyScalar(1.2); // adjust scalar factor to match your scene scale
+        car1.position.x = 1; // once rescaled, position the model where needed
+        car1.position.z = -35;
+        car1.position.y = 35;
+
+		scene.add( car1 );
 
 	}, undefined, function ( error ) {
 
@@ -87,72 +69,83 @@ function main() {
 
 	} );
     
+	loader.load( '../models/car2/scene.gltf', function(gltf){
+		car2 = gltf.scene;
+		car2.scale.multiplyScalar(30); // adjust scalar factor to match your scene scale
+        car2.position.x = 1; // once rescaled, position the model where needed
+        car2.position.z = -35;
+        car2.position.y = -25;
 
-	scene2.add(light2);
-	scene2.add(spotLight2);
-	scene2.add(directionalLight);
-	scene2.add(light);
-	loader.load( '../models/headphones/scene.gltf', function(gltf){
-		gltf.scene.scale.multiplyScalar(30); // adjust scalar factor to match your scene scale
-        gltf.scene.position.x = 1; // once rescaled, position the model where needed
-        gltf.scene.position.z = -35;
-        gltf.scene.position.y = -25;
+		scene.add(car2);
 
-		scene2.add(gltf.scene);
 	}, undefined, function (error) {
 		console.error(error);
 	});
 
 	
+	loader.load( '../models/car3/scene.gltf', function(gltf){
+		car3 = gltf.scene;
+		car3.scale.multiplyScalar(45); // adjust scalar factor to match your scene scale
+        car3.position.x = 1; // once rescaled, position the model where needed
+        car3.position.z = -35;
+        car3.position.y = 0;
 
-	scene3.add(light3);
-	scene3.add(spotLight3);
-	scene3.add(directionalLight);
-	scene3.add(light);
-	loader.load( '../models/gun/scene.gltf', function(gltf){
-		gltf.scene.scale.multiplyScalar(45); // adjust scalar factor to match your scene scale
-        gltf.scene.position.x = 1; // once rescaled, position the model where needed
-        gltf.scene.position.z = -35;
-        gltf.scene.position.y = 0;
+		scene.add(car3);
 
-		scene3.add(gltf.scene);
 	}, undefined, function (error) {
 		console.error(error);
 	});
 
+	init_buttons();
+	car3.visibile = false;
+	car2.visibile = false;
+// Alternatively, to parse a previously loaded JSON structure
+	animate();
+}
 
-
-
-
+function init_buttons(){
 	var backgroundButton = document.getElementById('scene');
 	backgroundButton.addEventListener('click', function(){
-		currentScene = scene;
+	
    		
 	});
 
 	var backgroundButton = document.getElementById('scene2');
 	backgroundButton.addEventListener('click', function(){
-		currentScene = scene2;
+		
    		
 	});
 
 	var backgroundButton = document.getElementById('scene3');
 	backgroundButton.addEventListener('click', function(){
-		currentScene = scene3;
+		
    		
 	});
-
-
-// Alternatively, to parse a previously loaded JSON structure
-	currentScene = scene;
-	animate();
 }
 
+function keyPressed(e){
+  switch(e.key) {
+    case 'ArrowUp':
+        currentScene.rotateX(-0.05);
+        break;
+    case 'ArrowDown':
+        currentScene.rotateX(0.05);
+        break;
+    case 'ArrowLeft':
+        currentScene.rotateY(-0.05);
+        break;
+    case 'ArrowRight':
+        currentScene.rotateY(0.05);
+        break;
+  }
+  e.preventDefault();
+  render();
+}
 
 function animate(){
 	requestAnimationFrame(animate);
 	//directionalLight.position.copy(camera.position)
-	renderer.render(currentScene, camera);
+	renderer.render(scene, camera);
 }
 
 
